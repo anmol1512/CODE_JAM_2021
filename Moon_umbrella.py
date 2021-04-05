d3c80297f57b5c209fc566812f6ed15586e68138
Anmol Chhetri <64525986+anmol1512@users.noreply.github.com>
2021-04-05 16:04:51 +0530

    writing cost variable was much understandable

--- Moon_umbrella.py
+++ Moon_umbrella.py
@@ -4,5 +4,6 @@ for k in range(t):
     tcl=[int(x) if x.isdigit() else x for x in input().split()]
     mural=tcl[2]
     mural=mural.replace('?','x')
-    print('Case '+'#'+str(k+1)+':',mural.count('CJ')*tcl[0]+mural.count('JC')*tcl[1]+mural.count('CxJ')*tcl[0]+mural.count('JxC')*tcl[1]+len(re.findall("Cxx+J", mural))*tcl[0]+len(re.findall("Jxx+C", mural))*tcl[1])
+    cost=mural.count('CJ')*tcl[0]+mural.count('JC')*tcl[1]+mural.count('CxJ')*tcl[0]+mural.count('JxC')*tcl[1]+len(re.findall("Cxx+J", mural))*tcl[0]+len(re.findall("Jxx+C", mural))*tcl[1]
+    print('Case '+'#'+str(k+1)+':',cost)
         

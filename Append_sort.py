f28d8f9a2baf656d23259c209fd488aeca23b1d0
Anmol Chhetri <anmol1512@gmail.com>
2021-04-10 22:12:48 +0530

    Sample and Test case 1 passed(Round 1A)

--- Append_sort.py
+++ Append_sort.py
@@ -4,16 +4,22 @@ for k in range(t):
     l=[int(x) for x in input().split()]
     cost=0
     for i in range(1,n):
-        if l[i-1]>l[i]:
+        if l[i-1]>=l[i]:
             diff=len(str(l[i-1]))-len(str(l[i]))
             x=str(l[i-1])[-diff:]
-            if l[i]>int(str(l[i-1])[:len(str(l[i]))]):
-                l[i]=int(str(l[i])+x)
-                cost+=diff
+            if diff>0:
+                if l[i]>int(str(l[i-1])[:len(str(l[i]))]):
+                    l[i]=l[i]*(10**len(x))
+                    cost+=len(x)
+                elif l[i]==int(str(l[i-1])[:len(str(l[i]))]):
+                    x=str(int(x)+1)
+                    l[i]=int(str(l[i])+x)
+                    cost+=len(x)
+                else:
+                    l[i]=(l[i]*(10**len(x)))*10
+                    cost+=len(x)+1
             else:
-                l[i]=int(str(l[i])+x+'0')
-                cost+=diff+1
-        elif l[i-1]==l[i]:
-            l[i]=int(str(l[i])+'0')
-            cost+=1
+                l[i]=l[i]*10
+                cost+=1
+                    
     print('Case '+'#'+str(k+1)+':',cost)
